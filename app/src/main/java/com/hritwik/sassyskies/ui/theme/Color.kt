4a31f1adc05a58fc081ca712b062887c13eb04c1
Hritwik <hritwikjohri@gmail.com>
2025-06-07 22:07:45 +0530

    Color and theme added

--- app/src/main/java/com/hritwik/sassyskies/ui/theme/Color.kt
+++ app/src/main/java/com/hritwik/sassyskies/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.hritwik.sassyskies.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SkyBlue80 = Color(0xFF95d6ea)
+val AquaTeal80 = Color(0xFF7bc7dd)
+val SteelBlue80 = Color(0xFF62a1c7)
+val OceanBlue40 = Color(0xFF528ab4)
+val DeepBlue40 = Color(0xFF15719f)
--- app/src/main/java/com/hritwik/sassyskies/ui/theme/Theme.kt
+++ app/src/main/java/com/hritwik/sassyskies/ui/theme/Theme.kt
@@ -8,12 +8,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = SkyBlue80,
+    secondary = AquaTeal80,
+    tertiary = SteelBlue80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = OceanBlue40,
+    secondary = DeepBlue40,
+    tertiary = SteelBlue80
+)
 
 @Composable
 fun SassySkiesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -22,8 +34,9 @@ fun SassySkiesTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-        darkTheme -> darkColorScheme()
-        else -> lightColorScheme()
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
 
     MaterialTheme(
